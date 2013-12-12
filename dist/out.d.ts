1be56da2d23072c21ca5385020bd0a243b27b8a3
Chris <chris.glover@gmail.com>
2013-12-13 10:30:09 +1100

    fixing...

--- dist/out.d.ts
+++
@@ -1,9 +0,0 @@
-declare class Greeter {
-    public element: HTMLElement;
-    public span: HTMLElement;
-    public timerToken: number;
-    constructor(element: HTMLElement);
-    public start(): void;
-    public stop(): void;
-}
-declare function parseSVG(s: string): Node;
